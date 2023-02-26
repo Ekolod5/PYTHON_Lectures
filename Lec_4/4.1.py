0d4468bda5664dae2ca694156f2a1c8de2bf41fd
lena <ekolod5@mail.ru>
2023-02-26 17:25:00 +0300

    4.1

--- Lec_4/4.1.py
+++ Lec_4/4.1.py
@@ -92,8 +92,8 @@ def where(f, col):
     return [x for x in col if f(x)]
 
 data = [1, 2, 3, 5, 8, 15, 23, 38]
-res = select(int, data)                   # [1, 2, 3, 5, 8, 15, 23, 38]
-print(res)
+res = select(int, data)                  
+print(res)                                # [1, 2, 3, 5, 8, 15, 23, 38]
 res = where(lambda x: x % 2 == 0, res)
 print(res)                                # [2, 8, 38]
 res = list(select(lambda x: (x, x**2), res))
